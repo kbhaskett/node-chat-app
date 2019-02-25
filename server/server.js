67bd90bbcf19fa5122d38914def7f74d194a87c5
Kevin Haskett <kbhaskett@gmail.com>
2019-02-25 10:17:50 -0600

    adding socket listeners

--- server/server.js
+++ server/server.js
@@ -1,13 +1,25 @@
 const path = require('path');
 const express = require('express');
+const http = require('http');
+
+const socketIO = require('socket.io');
 
 const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "../public");
 
 var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+io.on('connection', (socket) => {
+    console.log("New user is connected");
+    socket.on('disconnect', () => {
+        console.log('page disconnected');
+    })
+});
 
 app.use(express.static(publicPath));
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`);
 });
